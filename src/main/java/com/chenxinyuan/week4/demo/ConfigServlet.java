d214b249d2a13c43c49d06562c965ab3defcc677
chenxinyuan <2837578655@qq.com>
2024-03-28 23:45:38 +0800

    HomeWork week4 Commit

---
+++ src/main/java/com/chenxinyuan/week4/demo/ConfigServlet.java
@@ -0,0 +1,57 @@
+package com.chenxinyuan.week4.demo;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name="ConfigDemoServlet",urlPatterns = "/config",
+        initParams ={
+                @WebInitParam(name="name1",value="GaoWeaning"),
+                @WebInitParam(name="studentID1",value = "2022211001000404")
+        }
+
+)
+
+public class ConfigServlet extends HttpServlet {
+
+
+    @Override
+    public void init() throws ServletException {
+
+
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletConfig config=getServletConfig();
+        String name= config.getInitParameter("name");
+        String id= config.getInitParameter("studentID");
+        String name1=config.getInitParameter("name1");
+        String id1=config.getInitParameter("studentID1");
+        PrintWriter writer=response.getWriter();
+        writer.println("<html><body>");
+        writer.println("<h1>" + "Task 1-Get init parameters from web.xml" + "</h1>");
+        writer.println("<br>name: "+name);
+        writer.println("<br>studentID: "+id);
+        writer.println("<br><h1>"+"Task 2-Get init parameters from @WebServlet"+"</h1>");
+        writer.println("<br>name1:"+name1);
+        writer.println("<br>studentID1:"+id1);
+        writer.println("</body></html>");
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
+    }
+
+
+}
